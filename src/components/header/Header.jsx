962678066a378c7d1c091897599740435eb3be5e
Nurgul Yunusova <nurgulyunusova03@gmail.com>
2024-03-12 10:11:43 +0400

    add new task button updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,41 @@
+import { useEffect, useRef, useState } from "react";
 import "./header.scss";
+import xmark from "../../assets/images/x-mark.svg";
 
 function Header() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [columnNames, setColumnNames] = useState(["", ""]);
+  const [selectedValue, setSelectedValue] = useState("");
+  const modalRef = useRef();
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+  const handleAddNewTask = () => {
+    setModalVisible(true);
+  };
+
+  const handleAddSubtask = () => {
+    setColumnNames([...columnNames, ""]);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setModalVisible(false);
+      }
+    };
+
+    if (modalVisible) {
+      window.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      window.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [modalVisible]);
+
   return (
     <>
       <header>
@@ -21,7 +56,79 @@ function Header() {
           </div>
 
           <div className="rightSide">
-            <button>+ Add New Task</button>
+            <button onClick={handleAddNewTask}>+ Add New Task</button>
+
+            {modalVisible && (
+              <div className="modalBackdrop">
+                <div className="modal" ref={modalRef}>
+                  <h3>Add New Task</h3>
+                  <form onSubmit={(e) => e.preventDefault()}>
+                    <label htmlFor="name">Task Name</label> <br />
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      placeholder="e.g. Take coffee break"
+                      required
+                    />{" "}
+                    <br />
+                    <label htmlFor="description">Description</label> <br />
+                    <textarea
+                      name="description"
+                      id="description"
+                      rows="7"
+                    ></textarea>
+                    <label htmlFor="boardColumns">Subtasks</label>
+                    {columnNames.map((columnName, index) => (
+                      <div key={index}>
+                        <input
+                          type="text"
+                          name={`column-${index}`}
+                          id={`column-${index}`}
+                          value={columnName}
+                          onChange={(e) => {
+                            const updatedColumns = [...columnNames];
+                            updatedColumns[index] = e.target.value;
+                            setColumnNames(updatedColumns);
+                          }}
+                          required
+                        />
+                        {
+                          <img
+                            src={xmark}
+                            alt="xmark"
+                            onClick={() => {
+                              const updatedColumns = [...columnNames];
+                              updatedColumns.splice(index, 1);
+                              setColumnNames(updatedColumns);
+                            }}
+                          />
+                        }
+                      </div>
+                    ))}
+                    <button
+                      className="addNewSubtaskBtn"
+                      onClick={handleAddSubtask}
+                    >
+                      + Add New Subtask
+                    </button>{" "}
+                    <br />
+                    <label htmlFor="status">Current Status</label> <br />
+                    <select
+                      id="selectOption"
+                      value={selectedValue}
+                      onChange={handleChange}
+                    >
+                      <option value="option1">Todo</option>
+                      <option value="option2">Doing</option>
+                      <option value="option3">Done</option>
+                    </select>
+                    <button className="createTaskBtn">Create Task</button>
+                  </form>
+                </div>
+              </div>
+            )}
+
             <div className="menu">
               <svg width="5" height="20" xmlns="http://www.w3.org/2000/svg">
                 <g fill="#828FA3" fillRule="evenodd">
