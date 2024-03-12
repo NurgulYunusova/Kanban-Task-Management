23d2f995123423dc1a7ac4c0799fd4d33c520634
Nurgul Yunusova <nurgulyunusova03@gmail.com>
2024-03-12 12:35:16 +0400

    menu button updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,55 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useEffect, useRef, useState } from "react";
 import "./header.scss";
 import xmark from "../../assets/images/x-mark.svg";
 
 function Header() {
   const [modalVisible, setModalVisible] = useState(false);
+  const [editModalVisible, setEditModalVisible] = useState(false);
   const [columnNames, setColumnNames] = useState(["", ""]);
+  const [editColumnNames, setEditColumnNames] = useState([
+    "Todo",
+    "Doing",
+    "Done",
+  ]);
   const [selectedValue, setSelectedValue] = useState("");
+  const [isEditOpen, setIsEditOpen] = useState(false);
+  const [isDeleteOpen, setIsDeleteOpen] = useState(false);
   const modalRef = useRef();
+
+  const openDeleteModal = () => {
+    setIsDeleteOpen(true);
+    setIsEditOpen(false);
+  };
+
+  const closeDeleteModal = () => {
+    setIsDeleteOpen(false);
+  };
+
+  const handleDelete = () => {
+    // Implement your delete logic here
+    // For example, you can make an API call to delete the board
+    // Once the delete operation is successful, close the modal
+    console.log("Deleting board...");
+    // Close the modal
+    closeDeleteModal();
+  };
+
+  const handleEdit = () => {
+    setEditModalVisible(true);
+    setIsEditOpen(false);
+  };
+
+  const handleAddColumn = () => {
+    setEditColumnNames([...editColumnNames, ""]);
+  };
 
   const handleChange = (event) => {
     setSelectedValue(event.target.value);
+  };
+
+  const toggleMenu = () => {
+    setIsEditOpen(!isEditOpen);
   };
 
   const handleAddNewTask = () => {
@@ -24,17 +64,18 @@ function Header() {
     const handleClickOutside = (event) => {
       if (modalRef.current && !modalRef.current.contains(event.target)) {
         setModalVisible(false);
+        setEditModalVisible(false);
       }
     };
 
-    if (modalVisible) {
+    if (modalVisible || editModalVisible) {
       window.addEventListener("mousedown", handleClickOutside);
     }
 
     return () => {
       window.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [modalVisible]);
+  }, [modalVisible, editModalVisible]);
 
   return (
     <>
@@ -56,7 +97,9 @@ function Header() {
           </div>
 
           <div className="rightSide">
-            <button onClick={handleAddNewTask}>+ Add New Task</button>
+            <button onClick={handleAddNewTask} className="addNewTaskBtn">
+              + Add New Task
+            </button>
 
             {modalVisible && (
               <div className="modalBackdrop">
@@ -130,13 +173,107 @@ function Header() {
             )}
 
             <div className="menu">
-              <svg width="5" height="20" xmlns="http://www.w3.org/2000/svg">
+              <svg
+                width="5"
+                height="20"
+                xmlns="http://www.w3.org/2000/svg"
+                onClick={toggleMenu}
+              >
                 <g fill="#828FA3" fillRule="evenodd">
                   <circle cx="2.308" cy="2.308" r="2.308" />
                   <circle cx="2.308" cy="10" r="2.308" />
                   <circle cx="2.308" cy="17.692" r="2.308" />
                 </g>
               </svg>
+
+              {isEditOpen && (
+                <div className="menuContent">
+                  <ul>
+                    <li className="edit" onClick={handleEdit}>
+                      Edit board
+                    </li>
+                    <li className="delete" onClick={openDeleteModal}>
+                      Delete board
+                    </li>
+                  </ul>
+                </div>
+              )}
+              {editModalVisible && (
+                <div className="modalBackdrop">
+                  <div className="modal" ref={modalRef}>
+                    <h3>Edit board</h3>
+                    <form onSubmit={(e) => e.preventDefault()}>
+                      <label htmlFor="name">Board Name</label> <br />
+                      <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="e.g. Web Design"
+                        value="Platform Launch"
+                        required
+                      />{" "}
+                      <br />
+                      <label htmlFor="boardColumns">Board Columns</label>
+                      {editColumnNames.map((columnName, index) => (
+                        <div key={index}>
+                          <input
+                            type="text"
+                            name={`column-${index}`}
+                            id={`column-${index}`}
+                            value={columnName}
+                            onChange={(e) => {
+                              const updatedColumns = [...editColumnNames];
+                              updatedColumns[index] = e.target.value;
+                              setEditColumnNames(updatedColumns);
+                            }}
+                            required
+                          />
+                          {
+                            <img
+                              src={xmark}
+                              alt="xmark"
+                              onClick={() => {
+                                const updatedColumns = [...editColumnNames];
+                                updatedColumns.splice(index, 1);
+                                setEditColumnNames(updatedColumns);
+                              }}
+                            />
+                          }
+                        </div>
+                      ))}
+                      <button
+                        className="addNewColumnBtn"
+                        onClick={handleAddColumn}
+                      >
+                        + Add New Column
+                      </button>{" "}
+                      <br />
+                      <button className="saveChangesBtn">Save Changes</button>
+                    </form>
+                  </div>
+                </div>
+              )}
+
+              {isDeleteOpen && (
+                <div className="deleteModal">
+                  <div className="modalContent">
+                    <h4>Delete this board?</h4>
+                    <p>
+                      Are you sure you want to delete the "Platform Launch"
+                      board? This action will remove all columns and tasks and
+                      cannot be reversed.
+                    </p>
+                    <div className="modalActions">
+                      <button onClick={handleDelete} id="delete">
+                        Delete
+                      </button>
+                      <button onClick={closeDeleteModal} id="cancel">
+                        Cancel
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
